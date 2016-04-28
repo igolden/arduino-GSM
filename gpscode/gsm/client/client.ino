57fc0ab95800c9cce2065e7abf4f94859a249e14
Paul Udehn <pludehn@yahoo.com>
2016-04-28 01:33:24 -0500

    Moved in some AT command based stuff.

--- gpscode/gsm/client/client.ino
+++ gpscode/gsm/client/client.ino
@@ -27,43 +27,46 @@ char inSerial[50];
 int i=0;
 boolean started=false;
 
+SoftwareSerial mySerial(10, 11); // RX, TX
+
 void setup()
 {
-     //Serial connection.
-     Serial.begin(9600);
-     Serial.println("GSM Shield testing.");
-     //Start configuration of shield with baudrate.
-     //For http uses is raccomanded to use 4800 or slower.
-     if (gsm.begin(2400)) {
-          Serial.println("\nstatus=READY");
-          started=true;
-     } else Serial.println("\nstatus=IDLE");
+  //Serial connection.
+  Serial.begin(9600);
+  Serial.println("GSM Shield testing.");
+  //Start configuration of shield with baudrate.
+  //For http uses is raccomanded to use 4800 or slower.
+  if (gsm.begin(2400)) {
+    Serial.println("\nstatus=READY");
+    started=true;
+  } else Serial.println("\nstatus=IDLE");
 
-     if(started) {
-          //GPRS attach, put in order APN, username and password.
-          //If no needed auth let them blank.
-          if (inet.attachGPRS("broadband", "", ""))
-               Serial.println("status=ATTACHED");
-          else Serial.println("status=ERROR");
-          //delay(1000);
+  if(started) {
+    //GPRS attach, put in order APN, username and password.
+    //If no needed auth let them blank.
+    if (inet.attachGPRS("broadband", "", ""))
+      Serial.println("status=ATTACHED");
+    else Serial.println("status=ERROR");
+    //delay(1000);
 
-          //Read IP address.
-          //gsm.SimpleWriteln("AT+CIFSR");
-          //delay(5000);
-          //Read until serial buffer is empty.
-          //gsm.WhileSimpleRead();
+    //Read IP address.
+    //gsm.SimpleWriteln("AT+CIFSR");
+    //delay(5000);
+    //Read until serial buffer is empty.
+    //gsm.WhileSimpleRead();
 
+ sendcoordinates();
+//
 
           Serial.println("starting connection...");
           //TCP Client GET, send a GET request to the server and
           //save the reply.
-          numdata=inet.httpGET("http://33f1f4a7.ngrok.io/handle_data.php?lat=41.948600&lon=-88.769332", 80, "", msg, 50);
+//          numdata=inet.httpGET("33f1f4a7.ngrok.io", 80, "/handle_data.php", "?lat=41.948600&lon=-88.769332", 50);
           //Print the results.
-          Serial.println("\nNumber of data received:");
-          Serial.println(numdata);
-          Serial.println("\nData received:");
-          Serial.println(msg);
-          Serial.println("Guess what, it worked!");
+ //         Serial.println("\nNumber of data received:");
+ //         Serial.println(numdata);
+ //         Serial.println("\nData received:");
+ //         Serial.println(msg);
      }
 };
 
@@ -112,4 +115,87 @@ void serialhwread()
 void serialswread()
 {
      gsm.SimpleRead();
+}
+
+void sendcoordinates()
+{
+  mySerial.println("AT+CGATT?");   //Attach or Detach from GPRS Service (Result  1 = Attach , 2 = Detached )
+  delay(300); 
+  ShowSerialData();
+
+  mySerial.println("AT+CIPSHUT=0");  //Close TCP Connection
+  delay(300); 
+  ShowSerialData();
+
+  mySerial.println("AT+SAPBR=3,1,\"broadband\",\"internet\"");//setting the APN, the second need you fill in your local apn server
+  delay(1000); 
+  ShowSerialData();
+
+  mySerial.println("AT+CIPSPRT=0");
+  delay(3000); 
+  ShowSerialData();
+
+  mySerial.println("AT+CIPSTART=\"TCP\",\"33f1f4a7.ngrok.io\",\"80\"");//start up the connection
+
+  delay(2000); 
+  ShowSerialData();
+  mySerial.println();
+  mySerial.println("AT+CIPSEND");//begin send data to remote server
+  delay(4500);
+  ShowSerialData();
+
+
+
+  mySerial.print("POST /handle_data.php?");//here is the feed you apply from pachube
+  delay(500);
+  ShowSerialData();
+
+  mySerial.print("lat=");   //DATA feed name
+  mySerial.print("41.123456");   //DATA to send
+  delay(10);
+  ShowSerialData();
+
+  mySerial.print("&lon=");   //DATA feed name
+  mySerial.print("-88.123456");   //DATA to send
+  delay(10);
+  ShowSerialData();
+
+
+  mySerial.print(" HTTP/1.1\r\n");
+  delay(500);
+  ShowSerialData();
+
+  mySerial.print("Host: 33f1f4a7.ngrok.io\r\n");
+  delay(500);
+  ShowSerialData();
+
+
+  mySerial.print("Connection: keep-alive");  //working
+  //mySerial.print("Connection: close");         //working as well
+  mySerial.print("\r\n");
+  mySerial.print("\r\n");
+  //mySerial.println();
+  delay(500);
+  ShowSerialData();
+
+
+  mySerial.println((char)26);//sending
+  mySerial.write(0x1A);
+  delay(2500);//waitting for reply, important! the time is base on the condition of internet 
+  mySerial.println(); 
+  ShowSerialData();
+
+  mySerial.println("AT+CIPCLOSE");//close the connection
+  delay(100);
+  ShowSerialData();
+
+  mySerial.println("AT+CIPSHUT=0");
+  delay(100); 
+  ShowSerialData();
+
+}
+void ShowSerialData()
+{
+  while(Serial.available()!=0)
+    Serial.write(Serial.read());
 }
--- gpscode/sticky_track/sticky_track.ino
+++ gpscode/sticky_track/sticky_track.ino
@@ -9,42 +9,60 @@
 // Init GSM
 InetGSM inet;
 
-//Serial Setup
+//Serial Setup - Client
 char msg[50];
 int numdata;
 char inSerial[50];
 int i=0;
 boolean started=false;
 
+// Setup test pin GPS
+int ledPin = 13;      
+
+// Set the variables
+float devLat=0;
+float devLon=0;
+
+// Construct the dGPS class
+dGPS dgps = dGPS();  
+
 void setup()
 {
+  // Init dGPS
+  pinMode(ledPin, OUTPUT);      
+  Serial.end();                
+  Serial.begin(9600);         
+  dgps.init();               
+
+
+
      //Serial connection.
      Serial.begin(9600);
      Serial.println("GSM Shield testing.");
-     //Start configuration of shield with baudrate.
-     //For http uses is raccomanded to use 4800 or slower.
+
      if (gsm.begin(2400)) {
           Serial.println("\nstatus=READY");
           started=true;
      } else Serial.println("\nstatus=IDLE");
 
      if(started) {
-          //GPRS attach, put in order APN, username and password.
-          //If no needed auth let them blank.
           if (inet.attachGPRS("broadband", "", ""))
                Serial.println("status=ATTACHED");
           else Serial.println("status=ERROR");
 
           Serial.println("starting connection...");
-          //TCP Client GET, send a GET request to the server and
-          //save the reply.
-          numdata=inet.httpGET("http://33f1f4a7.ngrok.io", 80, "/handle_data.php?lat=41.948600&lon=-88.769332", msg, 500);
+
+          //gsm.SimpleWriteln('AT+HTTPPARA=lat,41.948600');
+          //gsm.SimpleWriteln('AT+HTTPPARA=lat,41.948600');
+
+
+          // Make Request
+          numdata=inet.httpPOST("http://33f1f4a7.ngrok.io", 80, "/handle_data.php?lat=41.948600&lon=-88.769332", msg, 500);
           //Print the results.
           Serial.println("\nNumber of data received:");
           Serial.println(numdata);
           Serial.println("\nData received:");
           Serial.println(msg);
-          Serial.println("Guess what, it worked!");
      }
 };
 
@@ -91,4 +109,48 @@ void serialhwread()
 void serialswread()
 {
      gsm.SimpleRead();
+}
+void setup() {
+  pinMode(ledPin, OUTPUT);       // Initialize LED pin
+  Serial.end();                  // Close any previously established connections
+  Serial.begin(9600);            // Serial output back to computer.  On.
+  dgps.init();                   // Run initialization routine for dGPS.
+}
+
+void loop() {
+  
+  // This is where the GPS action is.
+  dgps.update(devLat, devLon);    // Calling this updates the GPS data.  The data in dGPS variables stays the same unless
+  Serial.print("Status: ");
+  Serial.println(dgps.Status());  // A - Satellites acquired and a valid signal.  V - No sats and not a valid signal.
+  
+  Serial.print("Latitude: ");
+  Serial.print(dgps.Lat(), 6);    // Lattitude - in DD.MMSSSS format (decimal-degrees format)  (D-Degree; M-Minute; S-Second)
+  Serial.println(" degrees");
+  
+  Serial.print("Longitude: ");
+  Serial.print(dgps.Lon(), 6);    // Longitude - in DD.MMSSSS format (decimal-degrees format)  (D-Degree; M-Minute; S-Second)
+  Serial.println(" degrees");
+  
+  // can get rid of the velocity, becuase we are updating every 5 seconds, unless we want to display the speed of the movement.
+  
+  Serial.print("Velocity: ");
+  Serial.print(dgps.Vel(), 6);    // Velocity, in knots.
+  Serial.println(" knots");
+  
+  // can probably comment azimuth out.
+  
+  Serial.print("Azimuth to destination: "); 
+  Serial.print(dgps.Azim());      //Azimuth of the destination coordinates from the current location in degrees. Correct upto 2 decimal points
+  Serial.println(" degrees");
+  
+
+  // This needs to be moved up above, the mode may be crucial, not sure.
+
+  Serial.print("Mode Indicator: "); 
+  Serial.println(dgps.Mode());     //Mode Indicator (N-Data not valid,A-Autonomous mode,D-Differential mode,E-Estimated mode,M-Manual input mode,S-Simulator mode
+
+// more user input... gotta make it all setup to run without user input.
+//put this here for test input
+  Serial.println("Loop is completed."); 
 }
